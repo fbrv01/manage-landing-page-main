1430855fd2080f907d05bd64c881719cfefb5dea
fbrv01 <fbucholc@gmail.com>
2022-08-18 06:32:45 +0200

    added manage section styles, testimonia section, carousel and mobile responsive

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ menuBtn.addEventListener('click', () => {
     menu.style.display = "flex"
     closeBtn.style.display = "inline-block"
     menuBtn.style.display = "none"
-    body.style.background= "linear-gradient(180deg, rgba(0, 0, 0, 0.0001) 0.33%, rgba(0, 0, 0, 0.599049) 99.79%)"
+    body.style.background = "linear-gradient(180deg, rgba(0, 0, 0, 0.0001) 0.33%, rgba(0, 0, 0, 0.599049) 99.79%)"
 })
 closeBtn.addEventListener('click', () => {
     menu.style.display = "none"
@@ -15,3 +15,38 @@ closeBtn.addEventListener('click', () => {
     menuBtn.style.display = "inline-block"
     body.style.background = "#FFF"
 })
+
+
+// SLIDER // 
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2.5,
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                renderBullet: function (index,className) {
+                    return `<span class=${className}></span>`;
+                }
+            },
+        },
+        1440: {
+            slidesPerView: 2.5,
+            spaceBetween: 30,
+            pagination: {
+                el: null
+            },
+        }
+    },
+});
